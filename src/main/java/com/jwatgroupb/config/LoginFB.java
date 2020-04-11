16b99c7cd4e0948eff906b3419be212cf21be1aa
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-04-11 22:39:04 +0700

    Update LoginFB.java

--- src/main/java/com/jwatgroupb/config/LoginFB.java
+++ src/main/java/com/jwatgroupb/config/LoginFB.java
@@ -17,9 +17,10 @@ public class LoginFB {
     public static String FACEBOOK_APP_SECRET = "dab3b3eb6d7fdf7e59a93732f4806871";
     public static String FACEBOOK_REDIRECT_URL = "https://testloginfb99.herokuapp.com/";
     public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
-
+    public String url="https://www.facebook.com/dialog/oauth?client_id=745329255998966&redirect_uri=https://testloginfb99.herokuapp.com/login-facebook";
     public String getToken(final String code) throws ClientProtocolException, IOException {
         String link = String.format(FACEBOOK_LINK_GET_TOKEN, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, FACEBOOK_REDIRECT_URL, code);
+        System.out.println("URL:"+link);
         String response = Request.Get(link).execute().returnContent().asString();
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(response).get("access_token");
