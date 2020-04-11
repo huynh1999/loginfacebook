ae709b571d95743ae3fa29db5db5fbddf6e52a64
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-04-11 23:00:07 +0700

    Update LoginFB.java

--- src/main/java/com/jwatgroupb/config/LoginFB.java
+++ src/main/java/com/jwatgroupb/config/LoginFB.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 public class LoginFB {
     public static String FACEBOOK_APP_ID = "745329255998966";
     public static String FACEBOOK_APP_SECRET = "dab3b3eb6d7fdf7e59a93732f4806871";
-    public static String FACEBOOK_REDIRECT_URL = "https://testloginfb99.herokuapp.com/";
+    public static String FACEBOOK_REDIRECT_URL = "https://testloginfb99.herokuapp.com/login-facebook";
     public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
     public String url="https://www.facebook.com/dialog/oauth?client_id=745329255998966&redirect_uri=https://testloginfb99.herokuapp.com/login-facebook";
     public String getToken(final String code) throws ClientProtocolException, IOException {
