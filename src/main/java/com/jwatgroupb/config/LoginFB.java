34d46a64beab1684129e79ddf94012e281ac5983
huynh1999 <55187798+huynh1999@users.noreply.github.com>
2020-04-12 10:41:19 +0700

    Update LoginFB.java

--- src/main/java/com/jwatgroupb/config/LoginFB.java
+++ src/main/java/com/jwatgroupb/config/LoginFB.java
@@ -54,8 +54,10 @@ public class LoginFB {
         boolean accountNonLocked = true;
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         authorities.add(new SimpleGrantedAuthority("customer"));
-        UserDetails userDetail = new MyUser(user.getName(), "", enabled, accountNonExpired, credentialsNonExpired,
+        UserDetails userDetail = new MyUser(user.getId(), "", enabled, accountNonExpired, credentialsNonExpired,
                 accountNonLocked, authorities);
+        ((MyUser)userDetail).setName(user.getName());
+        System.out.println(user.getName());
         return userDetail;
     }
 }
